28bc080d63510ec3b039190181e4f4b4897be24a
Echo545 <evan.er545@gmail.com>
2022-02-28 12:33:35 -0800

    restructure

---
+++ pi_test/adcTest.py
@@ -0,0 +1,12 @@
+# Analog to Digital Converter Test using MCP3008
+from MCP3008 import MCP3008
+import time
+
+adc = MCP3008()
+
+while True:
+
+    value = adc.read( channel = 0 )
+    # print("Applied voltage: %.2f" % (value / 1023.0 * 3.3) )
+    print("Applied voltage: %f" % value)
+    time.sleep(.1)
